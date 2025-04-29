03a3b0d655092c16bc99dd2a964f564203d5156d
Xianan Zhu <zhuxianan22@mails.ucas.ac.cn>
2025-04-29 20:58:19 +0800

    feat: complete decoder, alu and shifter modules

--- npc/playground/src/exu/shifter.scala
+++ npc/playground/src/exu/shifter.scala
@@ -1,19 +1,20 @@
-package cpu
+package cpu.exu
 
 import chisel3._
 import chisel3.util._
+import cpu.decode._
 
 class Shifter extends Module {
 	val io = IO(new Bundle {
 		val A = Input(UInt(32.W))
 		val shamt = Input(UInt(5.W))
-		val shiftop = Input(UInt(2.W))
+		val shiftop = AluType()
 		val result = Output(UInt(32.W))
 	})
 	
 	io.result := MuxLookup(io.shiftop, 0.U(32.W))(Seq(
-		0b00.U 	-> (io.A << io.shamt),
-		0b01.U 	-> (io.A >> io.shamt),
-		0b11.U 	-> (io.A.asSInt >> io.shamt).asUInt
+		AluType.sll 	-> (io.A << io.shamt),
+		AluType.srl 	-> (io.A >> io.shamt),
+		AluType.sra 	-> (io.A.asSInt >> io.shamt).asUInt
 	))
 }
