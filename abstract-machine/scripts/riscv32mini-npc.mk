f32dcd85ac6c19359163087074f564875f7f07eb
Xianan Zhu <zhuxianan22@mails.ucas.ac.cn>
2025-05-06 16:48:52 +0800

    feat: add riscv32mini-npc.mk for npc test

---
+++ abstract-machine/scripts/riscv32mini-npc.mk
@@ -0,0 +1,17 @@
+include $(AM_HOME)/scripts/isa/riscv.mk
+include $(AM_HOME)/scripts/platform/npc.mk
+
+export PATH := $(PATH):$(abspath $(AM_HOME)/tools/rvmini)
+CC = rvmini-gcc
+AS = rvmini-gcc
+CXX = rvmini-g++
+
+CFLAGS  += -DISA_H=\"riscv/riscv.h\"
+COMMON_CFLAGS += -march=rv32i_zicsr -mabi=ilp32  # overwrite
+LDFLAGS       += -melf32lriscv                   # overwrite
+
+AM_SRCS += riscv/npc/libgcc/div.S \
+           riscv/npc/libgcc/muldi3.S \
+           riscv/npc/libgcc/multi3.c \
+           riscv/npc/libgcc/ashldi3.c \
+           riscv/npc/libgcc/unused.c
