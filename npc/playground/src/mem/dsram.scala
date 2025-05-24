e3ce771862302f14d643c9c4aa1e0af13d9cdb11
Xianan Zhu <zhuxianan22@mails.ucas.ac.cn>
2025-05-24 17:14:50 +0800

    feat: Add dram with AXI4Lite

--- npc/playground/src/mem/dsram.scala
+++ npc/playground/src/mem/dsram.scala
@@ -4,44 +4,9 @@ import chisel3._
 import chisel3.util._
 import chisel3.util.random._
 import cpu.decode._
+import cpu._
 
-class DataSRAM extends Module {
-	val io = IO(new Bundle {
-		val valid = Input(Bool())
-		val ready = Output(Bool())
-		val wen = Input(Bool())
-		val addr = Input(UInt(32.W))
-		val wdata = Input(UInt(32.W))
-		val wmask = Input(UInt(8.W))
-		val rdata = Output(UInt(32.W))
-	})
-	val dpiMem = Module(new DPIMem())
-	val dataReg = RegEnable(dpiMem.io.rdata, io.valid)
-	dpiMem.io.valid := io.valid
-	dpiMem.io.wen := io.wen
-	dpiMem.io.addr := io.addr
-	dpiMem.io.wdata := io.wdata
-	dpiMem.io.wmask := io.wmask
-
-	val s_idle :: s_memready :: Nil = Enum(2)
-	val state = RegInit(s_idle)
-	state := MuxLookup(state, s_idle)(Seq(
-		s_idle -> Mux(io.valid && !io.wen, s_memready, s_idle),
-		s_memready -> Mux(io.valid && io.ready, s_idle, s_memready)
-	))
-
-
-	val cnt = RegInit(0.U(4.W))
-	val random = LFSR(4, io.valid, Some(BigInt(7))) // random latency
-	when(io.valid && !io.wen) {
-		cnt := random
-	}
-	when(state === s_memready && cnt =/= 0.U) {
-		cnt := cnt - 1.U
-	}
-	io.ready := state === s_memready && cnt === 0.U
-	io.rdata := dataReg
-
+trait memfunc {
 	/* Select because of aligned requirement */
 	def getAlignedAddr(addr: UInt, lstype: UInt): UInt = {
 		MuxLookup(lstype, 0.U(32.W))(Seq(
@@ -71,4 +36,50 @@ class DataSRAM extends Module {
 			LSLen.byte -> (Cat(0.U(7.W), "b1".U) << offset(1, 0))
 		))
 	}
+}
+
+class DataSRAM extends AXI4LiteBase with memfunc {
+	val dpiMem = Module(new DPIMem())
+	
+	// random access latency
+	val rcnt = RegInit(0.U(5.W))
+	val rlfsr = LFSR(5, io.arvalid && io.arready, Some(BigInt(0b00101)))
+	when(io.arvalid && io.arready) {
+		rcnt := rlfsr
+	}
+	when(rcnt =/= 0.U) {
+		rcnt := rcnt - 1.U
+	}
+	when(rcnt === 0.U) {
+		dpiMem.io.valid := true.B
+		dpiMem.io.wmask := io.wstrb
+		dpiMem.io.addr := raddr
+	}
+	io.arready := rstate === r_idle
+	io.rvalid := rstate === r_waitrdata && rcnt === 0.U
+	io.rdata := dpiMem.io.rdata
+	io.rresp := RespEncoding.OKAY
+
+	val wcnt = RegInit(0.U(5.W))
+	val wlfsr = LFSR(5, io.wvalid && io.wready, Some(BigInt(0b01001)))
+	when(io.arvalid && io.arready) {
+		wcnt := wlfsr
+	}
+	when(wastate === aw_fire && wstate === w_fire) {
+		when(wcnt =/= 0.U) {
+			wcnt := wcnt - 1.U
+		}
+		when(wcnt === 0.U) {
+			dpiMem.io.valid := true.B
+			dpiMem.io.wen := true.B
+			dpiMem.io.wmask := io.wstrb
+			dpiMem.io.addr := waddr
+			dpiMem.io.wdata := io.wdata
+		}
+	}
+
+	io.awready := wastate === aw_idle
+	io.wready := wstate === w_idle
+	io.bvalid := wastate === aw_fire && wstate === w_fire && wcnt === 0.U
+	io.bresp := RespEncoding.OKAY
 }
